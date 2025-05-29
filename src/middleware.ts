7e1525333bac24a6d7506e47618bffea131cbf48
anantariskys <gamersindo555@gmail.com>
2025-05-30 00:30:12 +0700

    style(middleware): standardize string quotes to single quotes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,24 @@
-import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|api|images).*)"], // Hindari file statis
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|api|images).*)'], // Hindari file statis
 };
 export default withAuth(
   function middleware(req) {
     const token = req.nextauth?.token;
-    const isLoginPage = req.nextUrl.pathname === "/login";
+    const isLoginPage = req.nextUrl.pathname === '/login';
 
-    console.log("Token:", token);
+    console.log('Token:', token);
 
     // Jika user sudah login dan mencoba buka /login, arahkan ke dashboard (/)
     if (isLoginPage && token) {
-      return NextResponse.redirect(new URL("/", req.url));
+      return NextResponse.redirect(new URL('/', req.url));
     }
 
     // Jika user belum login dan bukan sedang di halaman /login, arahkan ke /login
     if (!token && !isLoginPage) {
-      return NextResponse.redirect(new URL("/login", req.url));
+      return NextResponse.redirect(new URL('/login', req.url));
     }
 
     // Selain itu, izinkan akses
