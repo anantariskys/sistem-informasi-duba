f062771364c619da78b42a60b4799d49c20e8e07
anantariskys <gamersindo555@gmail.com>
2025-05-29 22:36:35 +0700

    style: standardize config files and update tailwind color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        'primary': '#022B3A',
-        'secondary': '#1F7A8C',
-        'tertiary': '#BFDBF7',
-        'custom-white' :'#E1E5F2',        
+        aquaMint: '#6FD0B4',
+        blueGradient: {
+          start: '#2D99DA',
+          end: '#64C2BF',
+        },
+        magentaPink: {
+          start: '#C62D83',
+          end: '#862B90',
+        },
+        darkPurple: '#901B81',
       },
     },
   },
   plugins: [],
-}
-
+};
